7b236ea35477819c53eba4e08234c7beb75be959
JuanGiacalone <juanpablogiacalone8@gmail.com>
2021-04-02 22:07:39 -0300

    added logo

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-      <router-link v-if="$auth.isAuthenticated" to="/profile">Profile</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link v-if="$auth.isAuthenticated" to="/profile">    Profile</router-link>
     </div>
     <router-view/>
   </div>
@@ -24,6 +24,7 @@
 
 #nav a {
   font-weight: bold;
+  text-decoration: none;
   color: #2c3e50;
 }
 
